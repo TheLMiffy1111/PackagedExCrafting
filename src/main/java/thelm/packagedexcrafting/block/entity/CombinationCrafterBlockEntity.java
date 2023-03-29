730021601055cfab5dbc0e154c88255da9a55f94
unknown <TheLMiffy1111@users.noreply.github.com>
2023-03-30 03:37:25 +0800

    Update CombinationCrafterBlockEntity.java

--- src/main/java/thelm/packagedexcrafting/block/entity/CombinationCrafterBlockEntity.java
+++ src/main/java/thelm/packagedexcrafting/block/entity/CombinationCrafterBlockEntity.java
@@ -54,7 +54,7 @@ public class CombinationCrafterBlockEntity extends BaseBlockEntity implements IP
 	public static int energyCapacity = 5000000;
 	public static boolean drawMEEnergy = false;
 
-	public int requiredPedestals = -1;
+	public int requiredPedestals = 0;
 	public boolean isWorking = false;
 	public long energyReq = 0;
 	public long remainingProgress = 0;
@@ -79,7 +79,7 @@ public class CombinationCrafterBlockEntity extends BaseBlockEntity implements IP
 		}
 		int availablePedestals = getEmptyPedestals().size();
 		MutableComponent message = new TranslatableComponent("block.packagedexcrafting.combination_crafter.pedestals.available", availablePedestals);
-		if(requiredPedestals >= 0) {
+		if(requiredPedestals > 0) {
 			message.append("\n");
 			message.append(new TranslatableComponent("block.packagedexcrafting.combination_crafter.pedestals.required", requiredPedestals));
 		}
