b78961cce43eb9cfa29321cdcdb6f0b6cded31df
unknown <TheLMiffy1111@users.noreply.github.com>
2023-03-31 02:05:48 +0800

    Changed message text

--- src/main/java/thelm/packagedexcrafting/block/entity/CombinationCrafterBlockEntity.java
+++ src/main/java/thelm/packagedexcrafting/block/entity/CombinationCrafterBlockEntity.java
@@ -77,8 +77,8 @@ public class CombinationCrafterBlockEntity extends BaseBlockEntity implements IP
 		if(isWorking) {
 			return null;
 		}
-		int availablePedestals = getEmptyPedestals().size();
-		MutableComponent message = new TranslatableComponent("block.packagedexcrafting.combination_crafter.pedestals.available", availablePedestals);
+		int usablePedestals = getEmptyPedestals().size();
+		MutableComponent message = new TranslatableComponent("block.packagedexcrafting.combination_crafter.pedestals.usable", usablePedestals);
 		if(requiredPedestals > 0) {
 			message.append("\n");
 			message.append(new TranslatableComponent("block.packagedexcrafting.combination_crafter.pedestals.required", requiredPedestals));
