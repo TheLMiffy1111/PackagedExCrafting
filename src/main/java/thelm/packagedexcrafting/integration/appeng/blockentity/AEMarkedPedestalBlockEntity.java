522842a247b119f710ea273a3bc189e2a60fa25d
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-03-02 03:01:01 +0800

    Made marked pedestals eject items to a ME system if it is connected to one
    Fix Combination Crafter progress bar
    Update recipes to use utility equals and hashCode methods

---
+++ src/main/java/thelm/packagedexcrafting/integration/appeng/blockentity/AEMarkedPedestalBlockEntity.java
@@ -0,0 +1,137 @@
+package thelm.packagedexcrafting.integration.appeng.blockentity;
+
+import appeng.api.config.Actionable;
+import appeng.api.features.IPlayerRegistry;
+import appeng.api.networking.GridHelper;
+import appeng.api.networking.IGrid;
+import appeng.api.networking.IGridNode;
+import appeng.api.networking.IGridNodeListener;
+import appeng.api.networking.IInWorldGridNodeHost;
+import appeng.api.networking.IManagedGridNode;
+import appeng.api.networking.energy.IEnergyService;
+import appeng.api.networking.security.IActionHost;
+import appeng.api.networking.storage.IStorageService;
+import appeng.api.stacks.AEItemKey;
+import appeng.api.storage.MEStorage;
+import appeng.api.storage.StorageHelper;
+import appeng.api.util.AECableType;
+import appeng.api.util.AEColor;
+import appeng.me.helpers.MachineSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import thelm.packagedexcrafting.block.MarkedPedestalBlock;
+import thelm.packagedexcrafting.block.entity.MarkedPedestalBlockEntity;
+
+public class AEMarkedPedestalBlockEntity extends MarkedPedestalBlockEntity implements IInWorldGridNodeHost, IGridNodeListener<AEMarkedPedestalBlockEntity>, IActionHost {
+
+	public boolean firstTick = true;
+	public MachineSource source;
+	public IManagedGridNode gridNode;
+
+	public AEMarkedPedestalBlockEntity(BlockPos pos, BlockState state) {
+		super(pos, state);
+		source = new MachineSource(this);
+	}
+
+	@Override
+	public void tick() {
+		if(firstTick) {
+			firstTick = false;
+			getMainNode().create(level, worldPosition);
+		}
+		super.tick();
+	}
+
+	@Override
+	public void setRemoved() {
+		super.setRemoved();
+		if(gridNode != null) {
+			gridNode.destroy();
+		}
+	}
+
+	@Override
+	public void onChunkUnloaded() {
+		super.onChunkUnloaded();
+		if(gridNode != null) {
+			gridNode.destroy();
+		}
+	}
+
+	@Override
+	public IGridNode getGridNode(Direction dir) {
+		return getActionableNode();
+	}
+
+	@Override
+	public AECableType getCableConnectionType(Direction dir) {
+		return AECableType.SMART;
+	}
+
+	@Override
+	public void onSaveChanges(AEMarkedPedestalBlockEntity nodeOwner, IGridNode node) {
+		setChanged();
+	}
+
+	public IManagedGridNode getMainNode() {
+		if(gridNode == null) {
+			gridNode = GridHelper.createManagedNode(this, this);
+			gridNode.setTagName("Node");
+			gridNode.setVisualRepresentation(MarkedPedestalBlock.INSTANCE);
+			gridNode.setGridColor(AEColor.TRANSPARENT);
+			gridNode.setIdlePowerUsage(1);
+			gridNode.setInWorldNode(true);
+			if(ownerUUID != null) {
+				gridNode.setOwningPlayerId(IPlayerRegistry.getMapping(level).getPlayerId(ownerUUID));
+			}
+		}
+		return gridNode;
+	}
+
+	@Override
+	public IGridNode getActionableNode() {
+		return getMainNode().getNode();
+	}
+
+	@Override
+	public void ejectItem() {
+		if(getMainNode().isActive()) {
+			IGrid grid = getMainNode().getGrid();
+			IStorageService storageService = grid.getStorageService();
+			IEnergyService energyService = grid.getEnergyService();
+			MEStorage inventory = storageService.getInventory();
+			ItemStack is = itemHandler.getStackInSlot(0);
+			if(!is.isEmpty()) {
+				AEItemKey key = AEItemKey.of(is);
+				int count = is.getCount();
+				int inserted = (int)StorageHelper.poweredInsert(energyService, inventory, key, count, source, Actionable.MODULATE);
+				if(inserted == count) {
+					itemHandler.setStackInSlot(0, ItemStack.EMPTY);
+				}
+				else {
+					itemHandler.setStackInSlot(0, key.toStack(count-inserted));
+				}
+			}
+		}
+		super.ejectItem();
+	}
+
+	@Override
+	public void load(CompoundTag nbt) {
+		super.load(nbt);
+		if(level != null && nbt.contains("Node")) {
+			getMainNode().loadFromNBT(nbt);
+		}
+	}
+
+	@Override
+	public void saveAdditional(CompoundTag nbt) {
+		super.saveAdditional(nbt);
+		if(gridNode != null) {
+			gridNode.saveToNBT(nbt);
+		}
+	}
+}
