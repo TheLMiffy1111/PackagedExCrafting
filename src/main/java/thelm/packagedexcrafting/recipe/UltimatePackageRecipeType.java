3060e00cae0b28c37d0a3e9d03e386219586d483
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-03-20 02:34:46 +0800

    Organize imports

--- src/main/java/thelm/packagedexcrafting/recipe/UltimatePackageRecipeType.java
+++ src/main/java/thelm/packagedexcrafting/recipe/UltimatePackageRecipeType.java
@@ -5,7 +5,6 @@ import java.util.stream.IntStream;
 
 import com.blakebr0.extendedcrafting.crafting.recipe.ShapelessTableRecipe;
 import com.blakebr0.extendedcrafting.init.ModBlocks;
-import com.google.common.collect.ImmutableList;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
