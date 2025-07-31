49015398fb757f54d063863ba3d45f861b7da92d
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-01 04:27:10 +0800

    Update Forge

--- src/main/java/thelm/packagedexcrafting/client/event/ClientEventHandler.java
+++ src/main/java/thelm/packagedexcrafting/client/event/ClientEventHandler.java
@@ -32,6 +32,7 @@ public class ClientEventHandler {
 		return INSTANCE;
 	}
 
+	@SuppressWarnings("removal")
 	public void onConstruct() {
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
 	}
--- src/main/java/thelm/packagedexcrafting/event/CommonEventHandler.java
+++ src/main/java/thelm/packagedexcrafting/event/CommonEventHandler.java
@@ -56,6 +56,7 @@ public class CommonEventHandler {
 	}
 
 	public void onConstruct() {
+		@SuppressWarnings("removal")
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.register(this);
 		PackagedExCraftingConfig.registerConfig();
